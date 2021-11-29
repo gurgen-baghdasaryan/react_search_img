80c494397d6040719bbcf91984f18b890781f094
gugohh <oohhgug@gmail.com>
2021-11-29 23:28:50 +0100

    calling api in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { useState } from 'react';
+
 import { Formik, Form, Field } from 'formik';
 import './header.css'
 
 
 function App() {
-
-
+  const [photos, setPhotos] = useState([])
+  console.log(photos);
   return (
     <>
       <header>
@@ -12,15 +14,21 @@ function App() {
           initialValues={{ search: '' }}
 
           onSubmit={async values => {
-            //llamar a API
-            console.log(values)
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+              headers: {
+                'Authorization': ' Client-ID fPlz8UEO_2tEcpOmeQnfv0-p2AfHf4WMHvJCqErbWEk'
+              }
+            })
+            const data = await response.json()
+            console.log(data)
           }}
-          >
-        <Form>
-          <Field name="search"></Field>
-        </Form>
+        >
+          <Form>
+            <Field name="search"></Field>
+          </Form>
         </Formik>
       </header>
+      
     </>
   );
 }
